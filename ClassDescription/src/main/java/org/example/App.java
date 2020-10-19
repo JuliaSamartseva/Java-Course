eb90939881cd4fd20156089245f91325d689c2be
Julia0212 <yulia.samartseva@gmail.com>
2020-10-19 16:18:12 +0300

    add class description class

--- ClassDescription/src/main/java/org/example/App.java
+++ ClassDescription/src/main/java/org/example/App.java
@@ -1,15 +1,15 @@
 package org.example;
 
+import java.util.logging.LogManager;
 import org.example.loader.CustomClassLoader;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws ClassNotFoundException {
-        CustomClassLoader loader = new CustomClassLoader(App.class.getClassLoader());
-        Class testClass = loader.loadClass("org.example.Test");
-    }
+public class App {
+  public static void main(String[] args) throws ClassNotFoundException {
+    LogManager.getLogManager().reset();
+
+    CustomClassLoader loader = new CustomClassLoader(App.class.getClassLoader());
+    Class testClass = loader.loadClass("org.example.SampleClass");
+    ClassDescription description = new ClassDescription(testClass);
+    description.printDescription();
+  }
 }
