01af0ddab956283e11378268b506b75045151e77
Julia Samartseva <yulia.samartseva@gmail.com>
2020-11-02 15:12:55 +0200

    XML parser (#8)
    * create XML parser skeleton, add XML and XSD files for parsing
    
    * add generated knife classes from XSD file
    
    * add DOM, SAX and StAX parsers
    
    * reformat code
    
    * add tests
    
    * change xml to include restricted size

---
+++ XMLParser/src/main/java/org/example/classes/Knives.java
@@ -0,0 +1,54 @@
+package org.example.classes;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(
+    name = "",
+    propOrder = {"knife"})
+@XmlRootElement(name = "knives")
+public class Knives implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @XmlElement(required = true)
+  protected List<Knife> knives;
+
+  /**
+   * Gets the value of the knife property.
+   *
+   * <p>This accessor method returns a reference to the live list, not a snapshot. Therefore any
+   * modification you make to the returned list will be present inside the JAXB object. This is why
+   * there is not a <CODE>set</CODE> method for the knife property.
+   *
+   * <p>For example, to add a new item, do as follows:
+   *
+   * <pre>
+   *    getKnife().add(newItem);
+   * </pre>
+   *
+   * <p>Objects of the following type(s) are allowed in the list {@link Knife }
+   */
+  public List<Knife> getKnives() {
+    if (knives == null) {
+      knives = new ArrayList<Knife>();
+    }
+    return this.knives;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder result = new StringBuilder();
+    for (Knife knife : knives) {
+      result.append(knife);
+    }
+    return result.toString();
+  }
+}
---
+++ XMLParser/src/main/java/org/example/validator/Validator.java
@@ -0,0 +1,23 @@
+package org.example.validator;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+public final class Validator {
+  public static boolean validateXMLSchema(String xsdPath, String xmlPath) {
+    try {
+      SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+      Schema schema = factory.newSchema(new File(xsdPath));
+      javax.xml.validation.Validator validator = schema.newValidator();
+      validator.validate(new StreamSource(new File(xmlPath)));
+    } catch (IOException | org.xml.sax.SAXException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+}
