6082e7d60bf5369f383cb19b7618033f711d9a56
Julia0212 <yulia.samartseva@gmail.com>
2020-11-15 22:26:20 +0200

    add implementation of lock-free skip list

--- SkipListFreeLock/src/main/java/org/example/App.java
+++ SkipListFreeLock/src/main/java/org/example/App.java
@@ -1,11 +1,6 @@
 package org.example;
 
-import java.util.concurrent.ConcurrentSkipListMap
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+  }
 }
